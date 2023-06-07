159f03415f463c665f98f432585e2fea4af0d422
rohith.sampathi <rohith.sampathi@gmail.com>
2023-06-07 11:53:53 +0530

    Structural Changes

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask, request, jsonify, render_template
 import openai
-from newspaper import Article
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import time
 
 app = Flask(__name__)
-openai.api_key = "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"
+openai.api_key = "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"
 
 
 @app.route('/')
@@ -17,76 +20,107 @@ def generate_route():
     theme = request.form['theme']
     try:
         news_content = process_url(url.strip())
+        if not news_content:
+            return jsonify({"error": f"Couldn't process URL {url}. Make sure the input is a valid URL with appropriate content."}), 400
+        print("Before generate function")
         news_snippet = generate(news_content, search_terms, theme)
+        print("After generate function")
         return jsonify({"result": {"art_output": news_snippet["result"]}})
     except Exception as e:
         return jsonify({"error": f"Error processing URL {url}: {str(e)}"}), 400
 
 def process_url(url):
-    article = Article(url)
-    article.download()
-    article.parse()
+    try:
+        options = Options()
+        options.add_argument('--headless')
+        driver = webdriver.Chrome(options=options)
 
-    headline = article.title
-    body = article.text[:2000]
+        print(f"URL: {url}")
+        driver.get(url)
+        time.sleep(10)  # Allow the page to load
+        html_content = driver.page_source
+        print(f"HTML content length: {len(html_content)}")
 
-    content = f"Heading: {headline}\n\nSnippet: {body}\n\n"
+        soup = BeautifulSoup(html_content, 'html.parser')
 
-    return content
+        headline = soup.find('h1').text.strip() if soup.find('h1') else "Not found"
+        print(f"Headline: {headline}")
+
+        # Search the body content and find all paragraphs
+        body = soup.body
+        paragraphs = body.find_all('p') if body else []
+        print(f"Paragraphs: {len(paragraphs)}")
+
+        # Handle NoneType text in case of empty paragraph tags
+        content_body = "\n\n".join([p.text.strip() if p.text is not None else "" for p in paragraphs])
+        print(f"Body: {content_body}")
+
+        content = f"Heading: {headline}\n\nSnippet: {content_body}\n\n"
+
+        driver.quit()
+
+        return content
+    except Exception as e:
+        print(f"Error processing URL {url}: {str(e)}")
+        return None
 
 
 def generate(news_content, search_terms, theme):
     writing_style = f"""
+        You are Montaigne, a writing assistant who learnt writing from Simon Sinek and greatly influenced by Steve Jobs product launch style. The [news content] is a summary of the product market research. Your purpose is to write a 750 word article from the news content to emphasize the {theme} of the given product market.
+        Writing Style:
+        1. Do not retain the structure of input. Modify the entire content and side headings to make the article highly professional and informative
+        2. Keep the content simple and business professional. Include latest trends and news. Include the [search terms] within the content. 
+        3. Target audience are business decision makers from procurement, product development, marketing, sales, management and R&D divisions.
+        4. Reader should find the article informative and request for a sample report of the [news-content] market from DataM Intelligence.
 
-    Your Background:
-    You are Montaigne a writing assistant who is a disciple of famous Martin Wolf and greatly influenced by Steve Jobs product launch style working for DataM Intelligence. The above content is a summary of the [product] market report. Write a 600 word article with Side headings of the [search terms] provided. 
+        MUST Follow rules:
+        1. Make the content 100% human-like. Rephrase the source content completely
+        2. Do not end the article abruptly. Do not exceed 800 words.
+        3. Do not use robotic sounding words and avoid using technical jargon. 
+        4. Add more details and examples to make the article highly informative.
+        5. Remove any references as a large language model or chatbot. "DataM Intelligence" alone should be quoted as reference in the entire article
 
-    Your Writing Style:
-    1. Purpose of this article is elaborate the [theme] and guide on what are the key trends around the [theme].
-    2. Keep the content simple and business professional. Cover the trends and news. Include the [search terms] within the content. 
-    3. Reader should feel empowered and request for download sample from DataM Intelligence to know more in depth information 
-    4. Target audience are business decision makers from procurement, product development, marketing, sales, management and R&D divisions.
+        Content Structure:
+        Heading Title [Should contain long tail keywords]
+        \n
+        Introduction paragraph giving the market overview
+        \n
+        Side Heading: Should include the phrases from {search_terms}
+        Content: Should elaborate the side headings using the text from [news content]
 
-    Follow the below Content Structure in the same order:
-    1. Heading Title [Should contain long tail keywords]
-    2. Introduction paragraph giving the market overview
-    3. Side Heading [Should contain the search terms]
-    4. Content from the summary [Use search terms and keyword as per SEO norms]
-    5. [Similarly for 3 more side headings]
-    6. Future Market Outlook [Redirect the reader to download-sample page]
+        [Similarly for more side headings]
 
-    Rules you MUST follow while writing:
-    1. Make the content 100% human-like. Rephrase the content and do not directly use reference information. 
-    2. Do not exceed 600 words. 
-    3. Do not use robotic sounding words and avoid using technical jargon. 
-    4. Add more details and examples to make the article highly informative.
-    5. Remove any references as a large language model or chatbot.
+        Future Market Outlook 
+        [Redirect the reader to download-sample page]
 
-    ------------------------- End of Instructions -------------------------
-
-    \n
-
+            ------------------------- End of Instructions -------------------------
 
     """
 
     # GPT-4 chat model processing with news_content as input
-    response = openai.ChatCompletion.create(
-        model="gpt-4",
-        messages=[
-            {"role": "system", "content": f"{writing_style}"},
-            {"role": "user", "content": f"{news_content} \n ------------- End of News Content News content -------------- \n Write the article from provided content. Please use the {search_terms} to write the article with the main focus on {theme} \n ---------- Start Writing Below -------- \n "},
-        ],
-        max_tokens=700,
-        top_p=1,
-        n=1,
-        stop=None,
-        temperature=0.8,
-    )
+    try:
+        print("Before GPT-4 API call")
+        response = openai.ChatCompletion.create(
+            model="gpt-4",
+            messages=[
+                {"role": "system", "content": f"{writing_style}"},
+                {"role": "user", "content": f"{news_content} \n ------------- End of News Content. Start Writing the Article Below -------- \n "},
+            ],
+            max_tokens=1250,
+            top_p=1,
+            n=1,
+            stop=None,
+            temperature=1.0,
+        )
+        print("After GPT-4 API call")
 
-    result = response.choices[0].message['content'].strip()
-    return {"result": result}
+        result = response.choices[0].message['content'].strip()
+        return {"result": result}
+    except Exception as e:
+        print(f"Error in generate function: {str(e)}")
+        return {"result": "An error occurred during generation"}
 
 if __name__ == '__main__':
     with app.app_context():
-        db.create_all()  # Creating all database tables
-    app.run(debug=True)
+        app.run(debug=True)
--- static/scripts.js
+++ static/scripts.js
@@ -1,75 +1,73 @@
 function updateElapsedTime(startTime) {
-    const elapsedTimeElement = document.getElementById("elapsed-time-value");
-    const currentTime = new Date();
-    const elapsedTime = Math.floor((currentTime - startTime) / 1000);
-    elapsedTimeElement.textContent = elapsedTime;
-  }
-  
-  function adjustHeight(element, defaultHeight) {
-    element.style.height = defaultHeight; // Reset element height in case of decreasing text lines
-    element.style.height = `${element.scrollHeight}px`; // Set element height equal to scroll height
-  }
-  
-  const dynamicHeightInputs = Array.from(document.getElementsByClassName("dynamic-height"));
-  dynamicHeightInputs.forEach(input => {
-    // Apply the height adjustment when the input changes
-    input.addEventListener('input', () => adjustHeight(input, '1em'));
-    // Initialize the correct height on page load
-    adjustHeight(input, '1em');
+  const elapsedTimeElement = document.getElementById("elapsed-time-value");
+  const currentTime = new Date();
+  const elapsedTime = Math.floor((currentTime - startTime) / 1000);
+  elapsedTimeElement.textContent = elapsedTime;
+}
+
+function adjustHeight(element, defaultHeight) {
+  element.style.height = defaultHeight; // Reset element height in case of decreasing text lines
+  element.style.height = `${element.scrollHeight}px`; // Set element height equal to scroll height
+}
+
+const dynamicHeightInputs = Array.from(document.getElementsByClassName("dynamic-height"));
+dynamicHeightInputs.forEach(input => {
+  // Apply the height adjustment when the input changes
+  input.addEventListener('input', () => adjustHeight(input, '1em'));
+  // Initialize the correct height on page load
+  adjustHeight(input, '1em');
+});
+
+const generateBtn = document.getElementById("generate-btn");
+if (generateBtn) {
+  generateBtn.addEventListener("click", async event => {
+    event.preventDefault();
+
+    const newsInput = document.getElementById("news-input");
+    const newsOutput = document.getElementById("news-output");
+    const loader = document.getElementById("loader");
+    const elapsedTimeContainer = document.getElementById("elapsed-time-container");
+
+    if (!newsInput.value) {
+      alert("Please enter a valid URL before generating the post.");
+      return;
+    }
+
+    loader.classList.remove("hidden");
+    elapsedTimeContainer.classList.remove("hidden");
+
+    const startTime = new Date();
+    updateElapsedTime(startTime);
+    const elapsedTimeInterval = setInterval(() => updateElapsedTime(startTime), 1000);
+
+    const formData = new FormData();
+    formData.append("news", newsInput.value);
+    formData.append("search_terms", document.getElementById("search-terms-input").value);
+    formData.append("theme", document.getElementById("theme-input").value);
+
+    try {
+      const response = await fetch("/generate", { method: "POST", body: formData });
+      const data = await response.json();
+      if (!response.ok) {
+        throw new Error(data.error ? data.error : "Network response was not ok");
+      }
+      loader.classList.add("hidden");
+      clearInterval(elapsedTimeInterval);
+      newsOutput.value = data.result.art_output;
+    } catch (error) {
+      console.error("Error:", error);
+      loader.classList.add("hidden");
+      elapsedTimeContainer.classList.add("hidden");
+      clearInterval(elapsedTimeInterval);
+    }
   });
-  
-  const generateBtn = document.getElementById("generate-btn");
-  if (generateBtn) {
-    generateBtn.addEventListener("click", async event => {
-      event.preventDefault();
-  
-      const newsInput = document.getElementById("news-input");
-      const newsOutput = document.getElementById("news-output");
-      const loader = document.getElementById("loader");
-      const elapsedTimeContainer = document.getElementById("elapsed-time-container");
-  
-      if (!newsInput.value) {
-        alert("Please enter some news before generating the post.");
-        return;
-      }
-  
-      loader.classList.remove("hidden");
-      elapsedTimeContainer.classList.remove("hidden");
-  
-      const startTime = new Date();
-      updateElapsedTime(startTime);
-      const elapsedTimeInterval = setInterval(() => updateElapsedTime(startTime), 1000);
-  
-      const formData = new FormData();
-      formData.append("news", newsInput.value);
-      formData.append("search_terms", document.getElementById("search-terms-input").value);
-      formData.append("theme", document.getElementById("theme-input").value);
-  
-      try {
-        const response = await fetch("/generate", { method: "POST", body: formData });
-        if (!response.ok) throw new Error("Network response was not ok");
-        const data = await response.json();
-  
-        loader.classList.add("hidden");
-        clearInterval(elapsedTimeInterval);
-        newsOutput.value = data.error ? data.error : data.result.art_output;
-      } catch (error) {
-        console.error("Error:", error);
-        loader.classList.add("hidden");
-        elapsedTimeContainer.classList.add("hidden");
-        clearInterval(elapsedTimeInterval);
-      }
-    });
-  }
-  
-  const regenerateBtn = document.getElementById("regenerate-btn");
-  if (regenerateBtn) {
-    regenerateBtn.addEventListener("click", () => {
-      document.getElementById("news-input").value = "";
-      document.getElementById("news-output").value = "";
-      generateBtn.click();
-    });
-  }
+}
 
-
-  
+const regenerateBtn = document.getElementById("regenerate-btn");
+if (regenerateBtn) {
+  regenerateBtn.addEventListener("click", () => {
+    document.getElementById("news-input").value = "";
+    document.getElementById("news-output").value = "";
+    generateBtn.click();
+  });
+}
